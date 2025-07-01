5db7f0dc741280a844a8fc1aaf27db1a6a552d8b
LateBloomer34 <himnariyal30@gmail.com>
2025-07-01 11:34:46 +0530

    count Digit in a number

---
+++ ProbSolving/day01_July01_Maths/CountDigitsInNumber.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+
+int countNum(int num){
+    int count = 0 ; 
+    if (num>0){
+    while(num>0){
+        count++;
+        num = num/10;
+    }
+}
+else if (num<0){
+    while(num<0){
+    count++;
+    num = num/10;
+}
+}
+else{
+    return 0;
+}
+    return count;
+}
+
+int main (){
+    int num; 
+    cout<<"enter the number:- ";
+    cin>>num;
+    cout<<countNum(num);
+    // cout<<num/10;
+}
