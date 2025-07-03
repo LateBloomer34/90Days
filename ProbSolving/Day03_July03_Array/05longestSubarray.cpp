de6f73ad6823eb51622764a6515961ddccbaf4f5
LateBloomer34 <himnariyal30@gmail.com>
2025-07-03 14:58:42 +0530

    longest subarray of given sum K(positive)

---
+++ ProbSolving/Day03_July03_Array/05longestSubarray.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+
+// int longestSubarraySum(vector<int>arr , int target){
+//     int n = arr.size();
+//     int sum = arr[0];
+//     int maxLength = 0;
+//     int i = 0 ; 
+//     int j = 0 ;
+//     int length = 0 ;
+//     while(i<n){
+//         while(i<=j && sum>target){
+//             sum = aum -arr[j];
+
+//         }       
+// }
+// }
+
+// int main (){
+//     vector<int>arr = {2,3,5,1,9};
+//     int target = 10;
+//    cout<< longestSubarraySum(arr, target);
+// }
