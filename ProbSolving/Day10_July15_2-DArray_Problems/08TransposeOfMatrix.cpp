e956f45b9506dbd8c4ecd68e8dd1bce7cc67dbd6
LateBloomer34 <himnariyal30@gmail.com>
2025-07-15 09:20:34 +0530

    transpose of matrix - swap row and column

--- ProbSolving/Day10_July15_2-DArray_Problems/08TransposeOfMatrix.cpp
+++ ProbSolving/Day10_July15_2-DArray_Problems/08TransposeOfMatrix.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+
+//method 1 -
+/*
 int transpose(vector<vector<int>>arr){
     int rowLength  = arr.size();
     int colLength = arr[0].size();
@@ -14,8 +17,36 @@ int transpose(vector<vector<int>>arr){
    
 return 0;
 }
+*/
+
+
+//method 2 -
+
+int transpose(vector<vector<int>> &arr){
+    int rowLength  = arr.size();
+    int colLength = arr[0].size();
+
+    for (int i = 0 ; i < rowLength ; i++){
+        for (int j = i ; j < colLength ; j++){
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+    // print the array
+
+     for (int i = 0 ; i < rowLength ; i++){
+        for (int j = 0 ; j < colLength ; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
 
 int main (){
-   vector<vector<int>>arr = {{10,12,34,42},{54,25,76,79},{37,84,90,56}};
+   vector<vector<int>>arr = {{10,12,34,42},{54,25,76,79},{37,84,90,56},{1,1,1,1}};
    transpose(arr);
 }
