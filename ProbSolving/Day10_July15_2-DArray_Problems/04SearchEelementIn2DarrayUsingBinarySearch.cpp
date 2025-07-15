6ee46ced59bc0b19a886903b90764054af2b9120
LateBloomer34 <himnariyal30@gmail.com>
2025-07-15 12:53:03 +0530

    find element in 2-D array using binary search

---
+++ ProbSolving/Day10_July15_2-DArray_Problems/04SearchEelementIn2DarrayUsingBinarySearch.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+pair<int , int> findElement(vector<vector<int>>arr, int target){
+    int start = 0 ; 
+    int end = arr.size()*arr[0].size()-1;
+    int rowIndex  = 0;
+    int colIndex = 0;
+    
+    while(start<=end){
+    int mid = start+(end-start)/2;
+    rowIndex = mid / arr[0].size();
+    colIndex = mid % arr[0].size();
+
+    if (arr[rowIndex][colIndex]==target){
+        cout<<"element present in array at index ";
+        return {rowIndex , colIndex};
+    }
+    else if (target>arr[rowIndex][colIndex]){
+        start = mid+1;
+    }
+    else{
+        end = mid-1;
+    }
+   }
+   cout<<"element not present in array ";
+return {-1,-1};
+}
+
+
+int main (){
+    vector<vector<int>>arr = { {10, 20, 30},{40, 50, 60},{70, 80, 90} };
+    int target = 70;
+    pair<int,int> result = findElement(arr , target);
+    cout<<result.first<<" "<<result.second;
+}
