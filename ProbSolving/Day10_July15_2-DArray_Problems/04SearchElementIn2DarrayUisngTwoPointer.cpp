7d6cf03d1564da424ec862105693a9c7f362ec59
LateBloomer34 <himnariyal30@gmail.com>
2025-07-15 13:16:18 +0530

    search element in 2-D array using two pointer

---
+++ ProbSolving/Day10_July15_2-DArray_Problems/04SearchElementIn2DarrayUisngTwoPointer.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+pair<int ,int> findEle(vector<vector<int>>arr, int target){
+        int start = 0 ; 
+        int end = arr.size();
+        int i = end-1;
+        int j = 0;
+
+        while(i>=0 && j<end){
+            if (arr[i][j]== target){
+                cout<<"target present at index ";
+                return {i, j};
+            }
+            else if (target>arr[i][j]){
+                j++;
+            }
+            else{
+                i--;
+            }
+        }
+cout<<"element not present ";
+return {-1, -1};    
+}
+
+int main (){
+    vector<vector<int>>arr = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+    int target = 55;
+    pair<int ,  int >result =  findEle(arr, target);
+    cout<< result.first<<" "<<result.second;
+}
