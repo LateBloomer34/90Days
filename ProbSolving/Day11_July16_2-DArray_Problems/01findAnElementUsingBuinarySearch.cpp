e4caef0a99a350ba7bee7d3fd5c8d9f6a656c8a7
LateBloomer34 <himnariyal30@gmail.com>
2025-07-16 10:53:17 +0530

    find element in 2D array using binary search

--- ProbSolving/Day11_July16_2-DArray_Problems/01findAnElementUsingBuinarySearch.cpp
+++ ProbSolving/Day11_July16_2-DArray_Problems/01findAnElementUsingBuinarySearch.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int findElement(vector<vector<int>>arr, int target){
-        int rowLen = arr.size();  // find the number of row in a matrix
+        int rowLen = arr.size();        // find the number of row in a matrix
         int colLen = arr[0].size();     // find the number of row in a matrix
 
-        int start = 0 ;         // find the starting index
+        int start = 0 ;                 // find the starting index
         int end = rowLen*colLen-1;      // find the elding index
 
 
         while (start <end){
             int mid = start +(end -start)/2;
-            int rowIndex = mid/colLen;  // calculate rowIndex
+            int rowIndex = mid/colLen;      // calculate rowIndex
             int colIndex = mid%colLen;      // calculate column index
             if (target == arr[rowIndex][colIndex]){
                 return 1;
