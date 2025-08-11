bf0af150c5a2505c7e5a35f21b193609fee6e4f4
LateBloomer34 <himnariyal30@gmail.com>
2025-08-11 12:28:53 +0530

    react using npm

---
+++ project/04_React/Day04_Aug11_25/first.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+let ele = (<h1>hello world! 2</h1>);
+let des = ReactDOM.createRoot(document.getElementById("root"));
+des.render(ele);
